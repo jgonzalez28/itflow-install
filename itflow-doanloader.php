1df7edb7bbd900aafc071d1f68792fec2f126775
jgonzalez28 <45581094+jgonzalez28@users.noreply.github.com>
2025-04-15 09:45:24 -0400

    Create itflow-doanloader.php
    Create script to clone the itflow repo.

---
+++ itflow-doanloader.php
@@ -0,0 +1,10 @@
+#!/usr/bin/env php
+
+<?php
+
+// Change to the directory of this script so that all shell commands run here
+//chdir(__DIR__);
+
+exec("sudo -u qrcash git clone https://github.com/jgonzalez28/itflow.git /home/qrcash/web/itdemo-test.quantizeit.com/public_html/ 2>&1", $output, $return_var);
+
+echo implode("\n", $output);
