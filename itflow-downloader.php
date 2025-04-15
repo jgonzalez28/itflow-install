12819d3a9a0b6d201adbd31e31102054b2c4bed2
jgonzalez28 <45581094+jgonzalez28@users.noreply.github.com>
2025-04-15 10:26:41 -0400

    Update itflow-downloader.php

--- itflow-downloader.php
+++ itflow-downloader.php
@@ -5,6 +5,6 @@
 // Change to the directory of this script so that all shell commands run here
 //chdir(__DIR__);
 
-exec("git clone https://github.com/jgonzalez28/itflow.git /home/qrcash/web/itdemo-test.quantizeit.com/public_html/ 2>&1", $output, $return_var);
+exec("git clone https://github.com/jgonzalez28/itflow.git /home/qrcash/web/itdemo-test.quantizeit.com/public_html/clone 2>&1", $output, $return_var);
 
 echo implode("\n", $output);
